5b452a380c9714581c1ccac3c935818a58ad680a
cn-tools <cn-tools@gmx.at>
2020-01-10 07:22:57 +0100

    Add support for URLs with playlist and user
    URL check by regex now

--- xExtension-YouTubeChannel2RssFeed/README.md
+++ xExtension-YouTubeChannel2RssFeed/README.md
@@ -2,6 +2,11 @@
 Transfer on the fly a YouTube-Channel into an RSS Feed.\
 Required FreshRSS version at least v1.16
 
+You can easily add Youtube video subscriptions by pasting URLs like:
+- `https://www.youtube.com/channel/UClvRFciRu04eKgti3g7qHUA`
+- `https://www.youtube.com/user/YouTube`
+- `https://www.youtube.com/playlist?list=PLcZ1vtdmuI2P7eZvbSq_d5mJXXywHtfyq`
+
 # Installation
 
 To install an extension, download the extension archive first and extract it on your PC.\
--- xExtension-YouTubeChannel2RssFeed/extension.php
+++ xExtension-YouTubeChannel2RssFeed/extension.php
@@ -2,16 +2,33 @@
 /*
 from: https://www.youtube.com/channel/UCLRLC0yPbRD-JaKzb5wKDdQ
 to: https://www.youtube.com/feeds/videos.xml?channel_id=UCLRLC0yPbRD-JaKzb5wKDdQ
+
+from: https://www.youtube.com/user/abcdefghijklmnop
+to: https://www.youtube.com/feeds/videos.xml?user=abcdefghijklmnop
+
+from: https://www.youtube.com/playlist?list=PLcZ1vtdmuI2P7eZvbSq_d5mJXXywHtfyq
+to: https://www.youtube.com/feeds/videos.xml?playlist_id=PLcZ1vtdmuI2P7eZvbSq_d5mJXXywHtfyq
+
 */
 class YouTubeChannel2RssFeedExtension extends Minz_Extension {
-	const CNT_YT_SEARCHSTR = 'youtube.com/channel/';
-	const CNT_YT_NEWSTR = 'youtube.com/feed/videos.xml?channel_id=';
-
 	public function init() {
 		$this->registerHook('check_url_before_add', array('YouTubeChannel2RssFeedExtension', 'CntYTRssHookCheckURL'));
 	}
 
 	public static function CntYTRssHookCheckURL($url) {
-		return str_replace(CNT_YT_SEARCHSTR, CNT_YT_NEWSTR, $url);
+        $matches = [];
+
+        if (preg_match('#^https?://(www\.|)youtube\.com/channel/([0-9a-zA-Z_-]{6,36})#', $url, $matches) === 1) {
+            return 'https://www.youtube.com/feeds/videos.xml?channel_id=' . $matches[2];
+        }
+
+        if (preg_match('#^https?://(www\.|)youtube\.com/user/([0-9a-zA-Z_-]{6,36})#', $url, $matches) === 1) {
+            return 'https://www.youtube.com/feeds/videos.xml?user=' . $matches[2];
+        }
+
+        if (preg_match('#^https?://(www\.|)youtube\.com/playlist/?\?list=([0-9a-zA-Z_-]{6,36})#', $url, $matches) === 1) {
+            return 'https://www.youtube.com/feeds/videos.xml?playlist_id=' . $matches[2];
+        }
+        return $url;
 	}
 }
