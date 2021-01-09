5a4a8fef86bb49e885468a24ecff2718d90110fb
Clemens Neubauer <cn-tools@gmx.at>
2021-01-09 08:48:46 +0100

    some little adaptations
    + add dynamic color of button `copy`
    * change to inline svg
    - remove file `copy.svg`

--- xExtension-Copy2Clipboard/extension.php
+++ xExtension-Copy2Clipboard/extension.php
@@ -9,14 +9,14 @@ class Copy2ClipboardExtension extends Minz_Extension {
 	}
 
     public static function CntJavascriptVars($vars){
-        $vars['copy2clipboard']['msg_good_one'] = _t('ext.Copy2Clipboard.messages.good_one');
-        $vars['copy2clipboard']['msg_good_more'] = _t('ext.Copy2Clipboard.messages.good_more');
-        $vars['copy2clipboard']['msg_bad'] = _t('ext.Copy2Clipboard.messages.bad');
+        $vars['copy2clipboard']['messages']['good_one'] = _t('ext.Copy2Clipboard.messages.good_one');
+        $vars['copy2clipboard']['messages']['good_more'] = _t('ext.Copy2Clipboard.messages.good_more');
+        $vars['copy2clipboard']['messages']['bad'] = _t('ext.Copy2Clipboard.messages.bad');
         return $vars;
     }
 
 	public static function CntCopy2Clipboard() {
-		$extension = Minz_ExtensionManager::findExtension('Copy2Clipboard');
-		return '<a id="copy2clipboard" class="btn" href="#" title="' . _t('ext.Copy2Clipboard.title') . '"><img class="icon" src="' . $extension->getFileUrl('copy.svg', 'svg') . '" alt="' . _t('ext.Copy2Clipboard.title') . '"></a>';
+        $svg = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path id="cnt_copy2clip_svg_path" d="M433.941 65.941l-51.882-51.882A48 48 0 0 0 348.118 0H176c-26.51 0-48 21.49-48 48v48H48c-26.51 0-48 21.49-48 48v320c0 26.51 21.49 48 48 48h224c26.51 0 48-21.49 48-48v-48h80c26.51 0 48-21.49 48-48V99.882a48 48 0 0 0-14.059-33.941zM266 464H54a6 6 0 0 1-6-6V150a6 6 0 0 1 6-6h74v224c0 26.51 21.49 48 48 48h96v42a6 6 0 0 1-6 6zm128-96H182a6 6 0 0 1-6-6V54a6 6 0 0 1 6-6h106v88c0 13.255 10.745 24 24 24h88v202a6 6 0 0 1-6 6zm6-256h-64V48h9.632c1.591 0 3.117.632 4.243 1.757l48.368 48.368a6 6 0 0 1 1.757 4.243V112z"/></svg>';
+		return '<a id="copy2clipboard" class="btn" href="#" title="' . _t('ext.Copy2Clipboard.title') . '"><div class="icon">' . $svg . '</div></a>';
 	}
 }
--- xExtension-Copy2Clipboard/static/copy2clipboard.js
+++ xExtension-Copy2Clipboard/static/copy2clipboard.js
@@ -3,20 +3,21 @@
 
 var cntCopy2Clipboard = {
 	init: function() {
-        var cntCopySelector = '#copy2clipboard';
-        var cntClipboard = new ClipboardJS(cntCopySelector, {
+		cnt_copy2clip_svg_path.style.fill = window.getComputedStyle(document.getElementsByClassName('btn')[0]).getPropertyValue('color');
+		
+        var cntClipboard = new ClipboardJS('#copy2clipboard', {
             text: function(trigger) {
+                let lResult = '';
                 let linkCount = 0;
                 let stream = document.getElementById('stream');
                 let links = stream.querySelectorAll('div.flux > ul > li.link > a');
-                let lResult = '';
                 links.forEach(function(item, index, arr){
                     if (lResult != ''){
                         lResult = lResult + '\n';
                     }
                     lResult = lResult + item['href'];
                     linkCount++;
-                }, lResult)
+                }, lResult);
                 trigger.setAttribute('data-lastcopycount', linkCount);
                 return lResult;
             }
@@ -26,10 +27,10 @@ var cntCopy2Clipboard = {
             console.info('FreshRSS - Copy2Clipboard: done');
             let msg = '';
             if (e.trigger.dataset.lastcopycount >> 1){
-                msg = context.extensions.copy2clipboard.msg_good_more;
+                msg = context.extensions.copy2clipboard.messages.good_more;
                 msg = msg.replace('#count#', e.trigger.dataset.lastcopycount.toString())
             } else {
-                msg = context.extensions.copy2clipboard.msg_good_one;
+                msg = context.extensions.copy2clipboard.messages.good_one;
             }
             openNotification(msg, 'good');
             e.clearSelection();
@@ -39,7 +40,7 @@ var cntCopy2Clipboard = {
             console.error('FreshRSS - Copy2Clipboard: Error');
             console.error('Action:', e.action);
             console.error('Trigger:', e.trigger);
-            openNotification(context.extensions.copy2clipboard.msg_bad, 'bad');
+            openNotification(context.extensions.copy2clipboard.messages.bad, 'bad');
         });
 
 	},
