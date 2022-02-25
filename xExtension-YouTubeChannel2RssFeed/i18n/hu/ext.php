87816ae86d93b1fdbc54a42d45c0396558b3a3a1
Clemens Neubauer <cn-tools@gmx.at>
2022-02-25 07:53:49 +0100

    New translations ext.php (Hungarian)

---
+++ xExtension-YouTubeChannel2RssFeed/i18n/hu/ext.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+	'YouTubeChannel2RssFeed' => array(
+        'install' => array(
+            'bad_freshrss' => 'Für "YouTubeChannel2RssFeed" ist mindestens die FreshRSS-Version `%s` erforderlich. (Sie verwenden die FreshRSS-Version `%s`)',
+        ),
+	),
+);
