3b69989cb78cbee07382d6eedc0b8720239dd87c
Clemens Neubauer <cn-tools@gmx.at>
2022-02-25 16:47:28 +0100

    New translations ext.php (English)

--- xExtension-Copy2Clipboard/i18n/en/ext.php
+++ xExtension-Copy2Clipboard/i18n/en/ext.php
@@ -17,8 +17,8 @@ return array(
         ),
         'messages' => array(
             'good_one' => 'One link was copied',
-            'good_more' => 'There have been copied #count# Links',
-            'bad' => 'An error occurred while copying!',
+            'good_more' => '#count# links were copied',
+            'bad' => 'An error occurred during copying!',
         ),
         'install' => array(
             'bad_freshrss' => '`Copy2Clipboard`required at least FreshRSS version `%s`. (You use FreshRSS version `%s`)',
