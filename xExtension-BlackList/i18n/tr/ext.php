65eb1f10e3b341661360fee27f3fe68191da9e40
Clemens Neubauer <cn-tools@gmx.at>
2022-02-25 07:52:51 +0100

    New translations ext.php (Turkish)

---
+++ xExtension-BlackList/i18n/tr/ext.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'black_list' => [
+        'blocked_patterns' => 'Blockierte Muster',
+        'warning' => [
+            'not_allowed_msg' => '"%s" ist ein gesperrtes Muster. Kontaktieren sie ihren Administrator für mehr Informationen.',
+        ],
+    ],
+];
