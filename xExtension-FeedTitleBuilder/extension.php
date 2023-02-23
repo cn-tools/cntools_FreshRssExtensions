9c8ac615f1355273a463e59e014a5d3d6ba152ec
Clemens Neubauer <cn-tools@gmx.at>
2023-02-23 12:33:45 +0100

    add try-catch and bump version

--- xExtension-FeedTitleBuilder/extension.php
+++ xExtension-FeedTitleBuilder/extension.php
@@ -19,29 +19,34 @@ class FeedTitleBuilderExtension extends Minz_Extension {
 	}
 
 	public static function CntBuildTitle($feed) {
-		if (is_object($feed) === true) {
-			$lWorkTitle = FreshRSS_Context::$user_conf->feedtitlebuilder["template"];
-			if ($lWorkTitle == '') {
-				// do nothing
-			} else {
-				// place original title
-				$lWorkTitle = str_replace('{origtitle}', $feed->name(), $lWorkTitle);
+        try {
+            if (is_object($feed) === true) {
+                $lWorkTitle = FreshRSS_Context::$user_conf->feedtitlebuilder["template"];
+                if ($lWorkTitle == '') {
+                    // do nothing
+                } else {
+                    // place original title
+                    $lWorkTitle = str_replace('{origtitle}', $feed->name(), $lWorkTitle);
 
-				// transfer url variables
-				self::$feedUrl = $feed->url();
-				$regex = "#{url\b(.*?)\}(.*?){/url}#s";
-				$lWorkTitle = preg_replace_callback($regex, array('FeedTitleBuilderExtension', 'renderTitleBuilderPhpParseUrl'), $lWorkTitle);
+                    // transfer url variables
+                    self::$feedUrl = $feed->url();
+                    $regex = "#{url\b(.*?)\}(.*?){/url}#s";
+                    $lWorkTitle = preg_replace_callback($regex, array('FeedTitleBuilderExtension', 'renderTitleBuilderPhpParseUrl'), $lWorkTitle);
 
-				// transfer date variables
-				$regex = "#{date\b(.*?)\}(.*?){/date}#s";
-				$lWorkTitle = preg_replace_callback($regex, array('FeedTitleBuilderExtension', 'renderTitleBuilderDate'), $lWorkTitle);
+                    // transfer date variables
+                    $regex = "#{date\b(.*?)\}(.*?){/date}#s";
+                    $lWorkTitle = preg_replace_callback($regex, array('FeedTitleBuilderExtension', 'renderTitleBuilderDate'), $lWorkTitle);
 
-				// send new title to feed
-				$feed->_name($lWorkTitle);
-			}
-		}
-		return $feed;
-	}
+                    // send new title to feed
+                    $feed->_name($lWorkTitle);
+                }
+            }
+            return $feed;
+        } catch (Exception $e) {
+            Minz_Log::error($e->getMessage());
+            return $feed;
+        }
+    }
 
 	/*---------------------------- renderTitleBuilderPhpParseUrl ----------------------------*/
 	private function renderTitleBuilderPhpParseUrl(&$matches){
@@ -59,5 +64,5 @@ class FeedTitleBuilderExtension extends Minz_Extension {
 	private function renderTitleBuilderDate(&$matches){
 		return date(trim($matches[2]));
 	}
-	
+
 }
