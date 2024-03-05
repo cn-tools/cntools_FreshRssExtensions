4ab85232a666b03949b653ef2897ea6ee6511c5c
Clemens Neubauer <cn-tools@gmx.at>
2024-03-05 21:38:18 +0100

    fix problen on save

--- xExtension-FilterTitle/extension.php
+++ xExtension-FilterTitle/extension.php
@@ -1,28 +1,31 @@
 <?php
 
 class FilterTitleExtension extends Minz_Extension {
-    public function init() {
+    public function init(): void {
         $this->registerTranslates();
 
         $this->registerHook('entry_before_insert', [$this, 'filterTitle']);
     }
-    
-    public function handleConfigureAction() {
+
+    public function handleConfigureAction(): void {
         $this->registerTranslates();
 
         if (Minz_Request::isPost()) {
             $configuration = [
-                'blacklist_title_keywords' => Minz_Request::paramTextToArray('blacklist_title_keywords', ''),
+                'blacklist_title_keywords' => Minz_Request::paramTextToArray('blacklist_title_keywords', []),
             ];
             $this->setSystemConfiguration($configuration);
         }
     }
 
     public function filterTitle($entry) {
-        foreach ($this->getSystemConfigurationValue('blacklist_title_keywords') as $pattern) {
-            if (1 === preg_match("/{$pattern}/i", $entry->title())) {
-                Minz_Log::warning(_t('ext.filter_title.warning.not_allowed_keyword', $entry->title()));
-                return null;
+        $patterns = $this->getSystemConfigurationValue('blacklist_title_keywords') ?? [];
+        if (is_array($patterns)) {
+            foreach ($patterns as $pattern) {
+                if (1 === preg_match("/{$pattern}/i", $entry->title())) {
+                    Minz_Log::warning(_t('ext.filter_title.warning.not_allowed_keyword', $entry->title()));
+                    return null;
+                }
             }
         }
         return $entry;
