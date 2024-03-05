c0cc67b38e5e38da554825d94b2f759c535ca493
Clemens Neubauer <cn-tools@gmx.at>
2024-03-05 21:48:16 +0100

    fix error on save

--- xExtension-BlackList/extension.php
+++ xExtension-BlackList/extension.php
@@ -1,28 +1,31 @@
 <?php
 
 class BlackListExtension extends Minz_Extension {
-    public function init() {
+    public function init(): void {
         $this->registerTranslates();
 
         $this->registerHook('check_url_before_add', [$this, 'checkBlackList']);
     }
     
-    public function handleConfigureAction() {
+    public function handleConfigureAction(): void {
         $this->registerTranslates();
 
         if (Minz_Request::isPost()) {
             $configuration = [
-                'blacklist_patterns' => Minz_Request::paramTextToArray('blacklist_patterns', ''),
+                'blacklist_patterns' => Minz_Request::paramTextToArray('blacklist_patterns', []),
             ];
             $this->setSystemConfiguration($configuration);
         }
     }
 
     public function checkBlackList($url) {
-        foreach ($this->getSystemConfigurationValue('blacklist_patterns') as $pattern) {
-            if (1 === preg_match("/{$pattern}/i", $url)) {
-                Minz_Log::warning(_t('ext.black_list.warning.not_allowed_msg', $url));
-                throw new FreshRSS_FeedNotAdded_Exception($url);
+        $patterns = $this->getSystemConfigurationValue('blacklist_patterns') ?? [];
+        if (is_array($patterns)) {
+            foreach ($patterns as $pattern) {
+                if (1 === preg_match("/{$pattern}/i", $url)) {
+                    Minz_Log::warning(_t('ext.black_list.warning.not_allowed_msg', $url));
+                    throw new FreshRSS_FeedNotAdded_Exception($url);
+                }
             }
         }
         return $url;
