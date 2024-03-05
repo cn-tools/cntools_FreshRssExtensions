cbbe935ec3166a949308ffeb5d8dcde46b9e0bb5
Clemens Neubauer <cn-tools@gmx.at>
2024-03-05 21:40:35 +0100

    Filter title regex (#15)
    * switch from string compare to preg_match regex
    
    * fix problen on save

--- xExtension-FilterTitle/extension.php
+++ xExtension-FilterTitle/extension.php
@@ -1,41 +1,37 @@
 <?php
 
 class FilterTitleExtension extends Minz_Extension {
-    public function init() {
+    public function init(): void {
         $this->registerTranslates();
 
         $this->registerHook('entry_before_insert', [$this, 'filterTitle']);
     }
-    
-    public function handleConfigureAction() {
+
+    public function handleConfigureAction(): void {
         $this->registerTranslates();
 
         if (Minz_Request::isPost()) {
             $configuration = [
-                'blacklist_title_keywords' => $this->parseKeywords(Minz_Request::paramString('blacklist_title_keywords')),
+                'blacklist_title_keywords' => Minz_Request::paramTextToArray('blacklist_title_keywords', []),
             ];
             $this->setSystemConfiguration($configuration);
         }
     }
 
-    private function parseKeywords($string) {
-        if (isset($string) && is_string($string)) {
-            return explode("\n", $string);
-        }
-        return [];
-    }
-
     public function filterTitle($entry) {
-        foreach ($this->getSystemConfigurationValue('blacklist_title_keywords') as $keyword) {
-            if (strpos($entry->title(), $keyword) !== false) {
-                Minz_Log::warning(_t('ext.filter_title.warning.not_allowed_keyword', $entry->title()));
-                return null;
+        $patterns = $this->getSystemConfigurationValue('blacklist_title_keywords') ?? [];
+        if (is_array($patterns)) {
+            foreach ($patterns as $pattern) {
+                if (1 === preg_match("/{$pattern}/i", $entry->title())) {
+                    Minz_Log::warning(_t('ext.filter_title.warning.not_allowed_keyword', $entry->title()));
+                    return null;
+                }
             }
         }
         return $entry;
     }
 
     public function getBlackKeywords() {
-        return implode("\n", $this->getSystemConfigurationValue('blacklist_title_keywords') ?? []);
+        return implode(PHP_EOL, $this->getSystemConfigurationValue('blacklist_title_keywords') ?? []);
     }
 }
