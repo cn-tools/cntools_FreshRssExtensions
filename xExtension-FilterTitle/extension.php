0afa11cdb9fa02ed86a2ece9a343c2df38c9fe3c
Clemens Neubauer <cn-tools@gmx.at>
2024-03-06 15:28:42 +0100

    add config parts and rework regex check

--- xExtension-FilterTitle/extension.php
+++ xExtension-FilterTitle/extension.php
@@ -12,6 +12,7 @@ class FilterTitleExtension extends Minz_Extension {
 
         if (Minz_Request::isPost()) {
             $configuration = [
+                'check_type' => Minz_Request::param('check_type', '0'),
                 'blacklist_title_keywords' => Minz_Request::paramTextToArray('blacklist_title_keywords', []),
             ];
             $this->setSystemConfiguration($configuration);
@@ -21,10 +22,25 @@ class FilterTitleExtension extends Minz_Extension {
     public function filterTitle($entry) {
         $patterns = $this->getSystemConfigurationValue('blacklist_title_keywords') ?? [];
         if (is_array($patterns)) {
+            $doRelease = (intval($this->getSystemConfigurationValue('blacklist_title_keywords', '0')) == 1);
             foreach ($patterns as $pattern) {
-                if (1 === preg_match("/{$pattern}/i", $entry->title())) {
-                    Minz_Log::warning(_t('ext.filter_title.warning.not_allowed_keyword', $entry->title()));
-                    return null;
+                if ($doRelease === true) {
+                    if (1 === preg_match($pattern, $entry->title())) {
+                        // do nothing
+                    } elseif (strpos($entry->title(), $patterns) !== false) {
+                        // do nothing
+                    } else {
+                        Minz_Log::warning(_t('ext.filter_title.warning.not_allowed_keyword', $entry->title()));
+                        return null;
+                    }
+                } else {
+                    if (strpos($entry->title(), $patterns) !== false) {
+                        Minz_Log::warning(_t('ext.filter_title.warning.not_allowed_keyword', $entry->title()));
+                        return null;
+                    } elseif (1 === preg_match($pattern, $entry->title())) {
+                        Minz_Log::warning(_t('ext.filter_title.warning.not_allowed_keyword', $entry->title()));
+                        return null;
+                    }
                 }
             }
         }
