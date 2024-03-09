ca188db9047256443ab3beb2a4798429304d744b
cn-tools <cn-tools@gmx.at>
2024-03-09 10:49:41 +0100

    rework descriptions

--- xExtension-FilterTitle/i18n/de/ext.php
+++ xExtension-FilterTitle/i18n/de/ext.php
@@ -3,9 +3,11 @@
 return array(
 	'filter_title' => array(
 		'keywords' => 'Schlüsselwörter',
-        'check_type' => 'Prüfungsart',
-        'block' => 'Blockieren',
-        'free' => 'Freigeben',
+		'keywords_desc' => 'Jede Zeile wird einzeln geprüft. Zusätzlich zu normalen Texten können auch RegEx-Expressions definieren werden. Diese müssen mit der PHP-Funktion `preg_match` ausgewertet werden können.',
+        'check_type' => 'Nutzen als',
+        'check_type_desc' => 'Hier legen sie fest, ob die angegebenen Schlüsselwörter als Blacklist oder als Whitelist genutzt werden sollen.        ',
+        'block' => 'Blacklist',
+        'free' => 'Whitelist',
         'warning' => [
             'not_allowed_msg' => '"%s" ist ein gesperrtes Schlüsselwort. Kontaktieren sie ihren Administrator für mehr Informationen.',
         ],
--- xExtension-FilterTitle/i18n/en/ext.php
+++ xExtension-FilterTitle/i18n/en/ext.php
@@ -3,9 +3,11 @@
 return array(
 	'filter_title' => array(
 		'keywords' => 'Keywords',
-        'check_type' => 'Exam type',
-        'block' => 'Block',
-        'free' => 'Release',
+		'keywords_desc' => 'Each line is checked individually. In addition to normal texts, RegEx expressions can also be defined. These must be able to be evaluated using the PHP function preg_match.',
+        'check_type' => 'Use as',
+        'check_type_desc' => 'Here you specify whether the specified keywords should be used as a blacklist or as a whitelist.',
+        'block' => 'Blacklist',
+        'free' => 'Whitelist',
         'warning' => [
             'not_allowed_msg' => '"%s" is a banned keyword. Contact your administrator for more information.',
         ],
