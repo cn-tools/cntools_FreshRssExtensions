a8bf70a156682c4340be4b2d1b7bbddf94db876a
cn-tools <cn-tools@gmx.at>
2024-05-06 10:42:54 +0200

    YouTube2RssFeed: add configuration page, dtet YT handels, detect YT shorts

--- xExtension-YouTubeChannel2RssFeed/README.md
+++ xExtension-YouTubeChannel2RssFeed/README.md
@@ -1,15 +1,76 @@
 # FreshRSS YouTubeChannel2RssFeed
-Transfer on the fly a YouTube-Channel into an RSS Feed.\
+
+Transfer on the fly a YouTube URL into an RSS Feed.\
 Required FreshRSS version at least v1.16
 
+You can easily add Youtube video subscriptions by pasting URLs.
+
+## Documentation
+
+The base idea behind creating this extension was to easily convert YouTube URLs into RSS feeds.
+
 You can easily add Youtube video subscriptions by pasting URLs like:
+
 - `https://www.youtube.com/channel/UClvRFciRu04eKgti3g7qHUA`
 - `https://www.youtube.com/user/YouTube`
 - `https://www.youtube.com/playlist?list=PLcZ1vtdmuI2P7eZvbSq_d5mJXXywHtfyq`
+- `https://www.youtube.com/@youtube` (by defining a third party service)
 
-# Installation
+In order to convert "YouTube Handels" into an RSS feed, third-party software is required and this must be an instance of [YouTube-operational-API](https://github.com/Benjamin-Loison/YouTube-operational-API). Please check the manufacturer's website to see if a publicly accessible instance is provided. Otherwise for example, you can host your own instance using a [Docker container](https://hub.docker.com/r/benjaminloison/youtube-operational-api).
 
-To install an extension, download the extension archive first and extract it on your PC.\
-Then, upload the specific extension(s) you want on your server.
+```yaml
+version: "3"
+services:
+  youtube-operational-api:
+    container_name: youtube-operational-api
+    image: benjaminloison/youtube-operational-api:latest
+    restart: unless-stopped
+    ports:
+      - "1234:80"
+```
+
+In addition, this third-party software makes it possible to identify YouTube shorts. Therefore, there is the option in the settings to mark these YouTube shorts as read when the RSS article is received or not to save it in the database at all. In order to be able to track it, a note is written to the FreshRSS LOG file in both of these cases.
+
+## Translations
+
+- English
+- German
+
+[![Crowdin](https://badges.crowdin.net/cntools-freshrssextensions/localized.svg)](https://crowdin.com/project/cntools-freshrssextensions)
+
+You can help me to translate my extensions to a couple of languages on [Crowdin](https://crowdin.com/project/cntools-freshrssextensions). Or send me a new translation as pull request. I am happy to see!
+
+## Installation
+
+To install an extension, download the extension archive first and extract it on your PC. Then, upload the specific extension(s) you want on your server.
 
 Extensions must be in the ./extensions directory of your FreshRSS installation.
+
+## Change log
+
+### v0.6.0-alpha (2024-05-06)
+
+- add option window
+- add detection of YouTube handels
+- add functionallity to detect YouTube shorts
+- add option to mark shorts as read or block it
+
+### v0.0.5 (2024-02-27)
+
+- modify function call of php `version_compare`
+
+### v0.0.4 (2023-12-02)
+
+- Removed third party url call from v0.0.3
+
+### v0.0.3 (2022-02-11)
+
+- Receive 50 instead of 15 items from YouTube by using third party service found on [stackoverflow.com](https://stackoverflow.com/questions/56430703/how-to-use-youtube-data-api-v3-to-get-more-than-15-videos-in-an-rss-reader-ne) by [Bman70](https://stackoverflow.com/users/7922428/bman70)
+
+### v0.0.2 (2020-01-10)
+
+- add support for playlist and user links
+
+### v0.0.1 (2019-12-06)
+
+- init version for YouTube channel links
--- xExtension-YouTubeChannel2RssFeed/extension.php
+++ xExtension-YouTubeChannel2RssFeed/extension.php
@@ -9,49 +9,198 @@ to: https://www.youtube.com/feeds/videos.xml?user=abcdefghijklmnop
 from: https://www.youtube.com/playlist?list=PLcZ1vtdmuI2P7eZvbSq_d5mJXXywHtfyq
 to: https://www.youtube.com/feeds/videos.xml?playlist_id=PLcZ1vtdmuI2P7eZvbSq_d5mJXXywHtfyq
 
+with third party url:
+from: https://www.youtube.com/@youtube
+to: https://www.youtube.com/feeds/videos.xml?channel_id=UCBR8-60-B28hp2BmDPdntcQ
 */
+
 class YouTubeChannel2RssFeedExtension extends Minz_Extension {
-	const CNT_REQUIRDED_FRESHRSS_VERSION = '1.16';
-	public static $channelID = '';
+    const CNT_REQUIRDED_FRESHRSS_VERSION = '1.16';
+    const CNT_YT_VIDEO = 'yt:video:';
+    const CNT_YT_FEEDURL = 'youtube.com/feeds/videos.xml?';
 
-	public function init() {
-		self::$channelID = '';
-		$this->registerHook('check_url_before_add', array('YouTubeChannel2RssFeedExtension', 'CntYTRssHookCheckURL'));
-		$this->registerHook('feed_before_insert', array('YouTubeChannel2RssFeedExtension', 'CntYTRssHookBeforeInsertFeed'));
-	}
+    public function init() {
+        $this->registerHook('check_url_before_add', [$this, 'CntYTRssHookCheckURL']);
+        $this->registerHook('entry_before_insert', [$this, 'CntYTRssHookEntryBeforeInsert']);
+    }
 
-	public function install() {
+    public function install() {
         if (version_compare(strval(FRESHRSS_VERSION), strval(self::CNT_REQUIRDED_FRESHRSS_VERSION), '<')){
             $this->registerTranslates();
             return _t('ext.YouTubeChannel2RssFeed.install.bad_freshrss', self::CNT_REQUIRDED_FRESHRSS_VERSION, FRESHRSS_VERSION);
         }
-		return true;
-	}
+        return true;
+    }
 
-	public static function CntYTRssHookBeforeInsertFeed($feed) {
-		if (self::$channelID != '') {
-			$lTxt = $feed->name() ? $feed->name() : 'YouTubeChannel2Rss by CN-Tools';
-			$url = 'https://www.youtube.com/feeds/videos.xml?channel_id=' . self::$channelID;
-			$feed->_url($url);
-		}
-		return $feed;
-	}
-	
-	public static function CntYTRssHookCheckURL($url) {
-        $matches = [];
+    public function handleConfigureAction() {
+        $this->registerTranslates();
 
-        if (preg_match('#^https?://(www\.|)youtube\.com/channel/([0-9a-zA-Z_-]{6,36})#', $url, $matches) === 1) {
-			self::$channelID = $matches[2];
-            return 'https://www.youtube.com/feeds/videos.xml?channel_id=' . $matches[2];
+        if (Minz_Request::isPost()) {
+            $config = [
+                '3rd_party_url' => rtrim(Minz_Request::paramString('3rd_party_url', ''), '/'),
+                'shorts' => Minz_Request::paramString('shorts', ''),
+                'entrytag' => Minz_Request::paramString('entrytag', ''),
+            ];
+
+            FreshRSS_Context::$user_conf->YouTubeChannel2RssFeed = $config;
+            FreshRSS_Context::$user_conf->save();
+        }
+    }
+
+    public static function CntYTRssHookCheckURL($url) {
+        Minz_Log::debug('YouTubeChannel2RssFeed-CntYTRssHookCheckURL: START - ' . $url);
+        if (stripos($url, self::CNT_YT_FEEDURL) === false) {
+            $origUrl = $url;
+            try {
+                $matches = [];
+
+                if (preg_match('#^https?://(www\.|)youtube\.com/channel/([0-9a-zA-Z_-]{6,36})#', $url, $matches) === 1) {
+                    Minz_Log::debug('YouTubeChannel2RssFeed-CntYTRssHookCheckURL: channel=' . $matches[2]);
+                    return 'https://www.youtube.com/feeds/videos.xml?channel_id=' . $matches[2];
+                }
+
+                if (preg_match('#^https?://(www\.|)youtube\.com/user/([0-9a-zA-Z_-]{6,36})#', $url, $matches) === 1) {
+                    Minz_Log::debug('YouTubeChannel2RssFeed-CntYTRssHookCheckURL: user=' . $matches[2]);
+                    return 'https://www.youtube.com/feeds/videos.xml?user=' . $matches[2];
+                }
+
+                if (preg_match('#^https?://(www\.|)youtube\.com/playlist/?\?list=([0-9a-zA-Z_-]{6,36})#', $url, $matches) === 1) {
+                    Minz_Log::debug('YouTubeChannel2RssFeed-CntYTRssHookCheckURL: playlist=' . $matches[2]);
+                    return 'https://www.youtube.com/feeds/videos.xml?playlist_id=' . $matches[2];
+                }
+
+                $extUrl = FreshRSS_Context::$user_conf->YouTubeChannel2RssFeed["3rd_party_url"];
+                if ($extUrl != ''){
+                    if (preg_match('#^https?://(www\.|)youtube\.com/([\@0-9a-zA-Z_-]{1,60})#', $url, $matches) === 1) {
+                        $origAllowUrlFopen = ini_get('allow_url_fopen');
+
+                        try {
+                            Minz_Log::debug('YouTubeChannel2RssFeed-CntYTRssHookCheckURL: 3rdParty - start');
+                            ini_set("allow_url_fopen", 1);
+                            $json = @file_get_contents($extUrl . '/channels?handle=' . $matches[2]) or throw new Exception(error_get_last()['message']);
+                            Minz_Log::debug('YouTubeChannel2RssFeed-CntYTRssHookCheckURL: 3rdParty - json=' . serialize($json));
+                        } finally {
+                            ini_set("allow_url_fopen", $origAllowUrlFopen);
+                            Minz_Log::debug('YouTubeChannel2RssFeed-CntYTRssHookCheckURL: 3rdParty - end');
+                        }
+
+                        $data = json_decode($json);
+                        $id = strval($data->items[0]->id);
+                        Minz_Log::debug('YouTubeChannel2RssFeed-CntYTRssHookCheckURL: 3rdParty=' . $matches[2]);
+                        return 'https://www.youtube.com/feeds/videos.xml?channel_id=' . $id;
+                    }
+                }
+            } catch (Exception $e) {
+                $url = $origUrl;
+                Minz_Log::error('YouTubeChannel2RssFeed-CheckURL: ' . $e->getMessage());
+            }
         }
 
-        if (preg_match('#^https?://(www\.|)youtube\.com/user/([0-9a-zA-Z_-]{6,36})#', $url, $matches) === 1) {
-            return 'https://www.youtube.com/feeds/videos.xml?user=' . $matches[2];
-        }
-
-        if (preg_match('#^https?://(www\.|)youtube\.com/playlist/?\?list=([0-9a-zA-Z_-]{6,36})#', $url, $matches) === 1) {
-            return 'https://www.youtube.com/feeds/videos.xml?playlist_id=' . $matches[2];
-        }
         return $url;
-	}
+    }
+
+    public static function CntYTRssHookEntryBeforeInsert($entry) {
+        Minz_Log::debug('YouTubeChannel2RssFeed-EntryBeforeInsert - START');
+        try {
+            if ((is_object($entry)) && (strpos($entry->guid(), self::CNT_YT_VIDEO) !== false)) {
+                Minz_Log::debug('YouTubeChannel2RssFeed-EntryBeforeInsert - isYT: ' . serialize($entry));
+                $externalUrl = strval(FreshRSS_Context::$user_conf->YouTubeChannel2RssFeed["3rd_party_url"]);
+                if ($externalUrl != '') {
+                    $shorts = strtolower(strval(FreshRSS_Context::$user_conf->YouTubeChannel2RssFeed["shorts"]));
+                    Minz_Log::debug('YouTubeChannel2RssFeed-EntryBeforeInsert: short-content = ' . $shorts);
+
+                    if ($shorts != '0') {
+                        $ytID = substr($entry->guid(), strlen(self::CNT_YT_VIDEO));
+                        Minz_Log::debug('YouTubeChannel2RssFeed-EntryBeforeInsert: before check isShort (id=' . $ytID . ')');
+                        if (self::isShort($entry, $externalUrl, $ytID) == true) {
+                            switch ($shorts) {
+                                case "block":
+                                    Minz_Log::debug('YouTubeChannel2RssFeed-EntryBeforeInsert: block short');
+                                    Minz_Log::notice('YouTubeChannel2RssFeed: short blocked (id=' . $ytID . ')');
+                                    return null;
+                                    break;
+                                case "mark_as_read":
+                                    Minz_Log::debug('YouTubeChannel2RssFeed-EntryBeforeInsert: mark short as read');
+                                    Minz_Log::notice('YouTubeChannel2RssFeed: mark short as read (id=' . $ytID . ')');
+                                    $entry->_isread(true);
+                                    return $entry;
+                                    break;
+                                default:
+                                    Minz_Log::debug('YouTubeChannel2RssFeed-EntryBeforeInsert: do nothing');
+                                    return $entry;
+                            }
+                        } else {
+                            Minz_Log::debug('YouTubeChannel2RssFeed-EntryBeforeInsert: is not short');
+                        }
+                    } else {
+                        Minz_Log::debug('YouTubeChannel2RssFeed-EntryBeforeInsert: no short check');
+                    }
+                }
+            }
+        } catch (Exception $e) {
+            Minz_Log::error('YouTubeChannel2RssFeed-EntryBeforeInsert: ' . $e->getMessage());
+        }
+
+        Minz_Log::debug('YouTubeChannel2RssFeed-EntryBeforeInsert - END');
+        return $entry;
+    }
+
+    private static function isShort($entry, $extUrl, $ytID): bool {
+        Minz_Log::debug('YouTubeChannel2RssFeed-isShort: START');
+        $result = false;
+        try {
+            if (is_object($entry)) {
+                // https://stackoverflow.com/questions/71192605/how-do-i-get-youtube-shorts-from-youtube-api-data-v3/71194751#71194751
+                Minz_Log::debug('YouTubeChannel2RssFeed-isShort: entry is OBJ - ID=' . $ytID);
+
+                $origAllowUrlFopen = ini_get('allow_url_fopen');
+
+                try {
+                    Minz_Log::debug('YouTubeChannel2RssFeed-isShort: fOpen - start');
+                    ini_set("allow_url_fopen", 1);
+                    $json = @file_get_contents($extUrl . '/videos?part=short&id=' . $ytID) or throw new Exception(error_get_last()['message']);
+                    Minz_Log::debug('YouTubeChannel2RssFeed-isShort: fOpen - data: ' . serialize($json));
+                } finally {
+                    ini_set("allow_url_fopen", $origAllowUrlFopen);
+                    Minz_Log::debug('YouTubeChannel2RssFeed-isShort: fOpen - end');
+                }
+
+                $data = json_decode($json);
+
+                if (isset($data) && isset($data->items) && (count($data->items) > 0)) {
+                    Minz_Log::debug('YouTubeChannel2RssFeed-isShort: items found: ' . strval(count($data->items)));
+                    foreach ($data->items as $item) {
+                        Minz_Log::debug('YouTubeChannel2RssFeed-isShort: item = ' . serialize($item));
+
+                        if (isset($item->id)) {
+                            Minz_Log::debug('YouTubeChannel2RssFeed-isShort: id is set');
+                            if ($item->id == $ytID) {
+                                Minz_Log::debug('YouTubeChannel2RssFeed-isShort: id found: ' . $ytID);
+                                if (isset($item->short)) {
+                                    Minz_Log::debug('YouTubeChannel2RssFeed-isShort: short is set');
+                                    if (isset($item->short->available)) {
+                                        Minz_Log::debug('YouTubeChannel2RssFeed-isShort: available is set');
+                                        if ($item->short->available == true) {
+                                            $result = true;
+                                            Minz_Log::debug('YouTubeChannel2RssFeed-isShort: THIS IS A SHORT');
+                                        } else {
+                                            $result = false;
+                                            Minz_Log::debug('YouTubeChannel2RssFeed-isShort: THIS IS NO SHORT');
+                                        }
+                                    }
+                                }
+                                break; // video was found => exit 'foreach'
+                            }
+                        }
+                    } // end of foreach
+                }
+            }
+        } catch (Exception $e) {
+            $result = false;
+            Minz_Log::error('YouTubeChannel2RssFeed-isShort: ' . $e->getMessage());
+        }
+
+        Minz_Log::debug('YouTubeChannel2RssFeed-isShort: isShort - END - result = ' . var_export($result, true));
+        return $result;
+    }
 }
