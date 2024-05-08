ff0087df275252e4e961f4985fa8bd25926f2803
Clemens Neubauer <cn-tools@gmx.at>
2024-05-08 19:46:13 +0200

    New translations ext.php (Chinese Simplified)

---
+++ xExtension-BlackList/i18n/zh/ext.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'black_list' => [
+        'blocked_patterns' => '已屏蔽用户',
+        'warning' => [
+            'not_allowed_msg' => '"%s" 是一个受限制的模式。请联系您的管理员获取更多信息。',
+        ],
+    ],
+];
