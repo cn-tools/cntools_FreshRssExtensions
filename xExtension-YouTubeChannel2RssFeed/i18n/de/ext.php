24eb52d09571b6f43fe5f5aa81f561be0812f5ba
Clemens Neubauer <cn-tools@gmx.at>
2024-05-08 19:51:54 +0200

    New translations ext.php (German)

--- xExtension-YouTubeChannel2RssFeed/i18n/de/ext.php
+++ xExtension-YouTubeChannel2RssFeed/i18n/de/ext.php
@@ -1,9 +1,36 @@
 <?php
 
 return array(
-	'YouTubeChannel2RssFeed' => array(
+    'YouTubeChannel2RssFeed' => array(
+        '3rd_party' => array(
+            'help' => array(
+                'lnk_txt' => 'Dokumentation',
+                'text' => 'Details über die Konfiguration und Funktionsweise finden sie hier:',
+            ),
+            'hint' => 'Mit Hilfe der anzugebenden URL können einerseits YoutTube Shorts identifiziert werden und andererseits können YouTube Handels in Feeds umgewandelt werden',
+            'instance' => 'Instanz von:',
+            'url' => 'URL',
+        ),
         'install' => array(
             'bad_freshrss' => 'Für "YouTubeChannel2RssFeed" ist mindestens die FreshRSS-Version `%s` erforderlich. (Sie verwenden die FreshRSS-Version `%s`)',
         ),
-	),
+        'shorts' => array(
+            'label' =>  'YouTube Shorts',
+            'hint' => 'Diese Einstellung wird nur berücksichtigt, wenn eine Instanz-URL hinterlegt ist. Ist keine hinterlegt, werden Shorts erlaubt',
+            'options' => array(
+                'default' => array(
+                    'text' => 'Standard',
+                    'title' => 'Keine Prüfung auf Youtube Shorts und der Feed-Eintrag wird ganz normal in die Datenbank aufgenommen',
+                ),
+                'block' => array(
+                    'text' => 'Blockieren',
+                    'title' => 'Wird der neue Feed-Eintrag als Youtube Short erkannt, wird dieser nicht in die Datenbank aufgenommen',
+                ),
+                'mark_as_read' => array(
+                    'text' => 'Als gelesen markieren',
+                    'title' => 'Wird der neue Feed-Eintrag als Youtube Short erkannt, wird dieser als gelesen markiert',
+                ),
+            ),
+        ),
+    ),
 );
