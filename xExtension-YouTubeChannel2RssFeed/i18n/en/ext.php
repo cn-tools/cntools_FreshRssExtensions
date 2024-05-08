5ba4008b7c5142b380891efceca613e481f4a3ae
Clemens Neubauer <cn-tools@gmx.at>
2024-05-08 19:51:56 +0200

    New translations ext.php (English)

--- xExtension-YouTubeChannel2RssFeed/i18n/en/ext.php
+++ xExtension-YouTubeChannel2RssFeed/i18n/en/ext.php
@@ -1,9 +1,36 @@
 <?php
 
 return array(
-	'YouTubeChannel2RssFeed' => array(
+    'YouTubeChannel2RssFeed' => array(
+        '3rd_party' => array(
+            'help' => array(
+                'lnk_txt' => 'Documentation',
+                'text' => 'Details about the configuration and functionality can be found here:',
+            ),
+            'hint' => 'With the help of the URL to be specified, YouTube Shorts can be identified on the one hand and YouTube handels can be converted into feeds on the other',
+            'instance' => 'Instance of:',
+            'url' => 'URL',
+        ),
         'install' => array(
             'bad_freshrss' => '`YouTubeChannel2RssFeed`required at least FreshRSS version `%s`. (You use FreshRSS version `%s`)',
         ),
-	),
+        'shorts' => array(
+            'label' =>  'YouTube Shorts',
+            'hint' => 'This setting is only taken if an instance URL is stored. If none is deposited, shorts are permitted',
+            'options' => array(
+                'default' => array(
+                    'text' => 'Default',
+                    'title' => 'No check for YouTube Shorts and the feed entry is added to the database as normal',
+                ),
+                'block' => array(
+                    'text' => 'Block',
+                    'title' => 'If the new feed entry is recognized as a Youtube Short, it will not be added into the database',
+                ),
+                'mark_as_read' => array(
+                    'text' => 'Mark as read',
+                    'title' => 'If the new feed entry is recognized as a Youtube Short, it will be marked as read',
+                ),
+            ),
+        ),
+    ),
 );
