5b9c435fdf46c93fdb82676773588d1c035e7a73
Clemens Neubauer <cn-tools@gmx.at>
2024-05-11 16:19:43 +0200

    New Crowdin updates (#19)
    * Update source file ext.php
    
    * New translations ext.php (French)
    
    * New translations ext.php (German)
    
    * New translations ext.php (Chinese Simplified)
    
    * New translations ext.php (French)
    
    * New translations ext.php (German)
    
    * New translations ext.php (French)
    
    * New translations ext.php (French)
    
    * New translations ext.php (English)
    
    * Update source file ext.php
    
    * Update source file ext.php
    
    * New translations ext.php (French)
    
    * New translations ext.php (German)
    
    * New translations ext.php (English)

---
+++ xExtension-BlackList/i18n/fr/ext.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'black_list' => [
+        'blocked_patterns' => 'Patterns Bloqués',
+        'warning' => [
+            'not_allowed_msg' => '"%s" est un modèle bloqué. Contactez votre administrateur pour plus d\'informations.',
+        ],
+    ],
+];
