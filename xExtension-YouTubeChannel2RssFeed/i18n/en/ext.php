10f86a6905c0ca9a7cc22f92048a933935e185b5
cn-tools <cn-tools@gmx.at>
2024-05-29 10:50:33 +0200

    Improving user-friendliness

--- xExtension-YouTubeChannel2RssFeed/i18n/en/ext.php
+++ xExtension-YouTubeChannel2RssFeed/i18n/en/ext.php
@@ -20,19 +20,19 @@ return array(
                 'hint' => 'If you set a duration greater than 0, all shorter videos will be identified as short, even if YouTube says it is not short.',
             ),
             'label' =>  'YouTube Shorts',
-            'hint' => 'This setting is only taken if an instance URL is stored. If none is deposited, shorts are permitted',
+            'hint' => 'This setting is only taken if an instance URL is stored. If none is deposited, shorts are permitted.',
             'options' => array(
                 'default' => array(
                     'text' => 'Default',
-                    'title' => 'No check for YouTube Shorts and the feed entry is added to the database as normal',
+                    'title' => 'No check for YouTube Shorts and the feed entry is added to the database as normal.',
                 ),
                 'block' => array(
                     'text' => 'Block',
-                    'title' => 'If the new feed entry is recognized as a Youtube Short, it will not be added into the database',
+                    'title' => 'If the new feed entry is recognized as a Youtube Short, it will not be added into the database.',
                 ),
                 'mark_as_read' => array(
                     'text' => 'Mark as read',
-                    'title' => 'If the new feed entry is recognized as a Youtube Short, it will be marked as read',
+                    'title' => 'If the new feed entry is recognized as a Youtube Short, it will be marked as read.',
                 ),
             ),
         ),
