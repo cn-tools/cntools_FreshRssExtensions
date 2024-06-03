e24b32e77f0789674965b3365ade4524d7e293f9
Clemens Neubauer <cn-tools@gmx.at>
2024-06-03 07:28:45 +0200

    New translations ext.php (English)

--- xExtension-YouTubeChannel2RssFeed/i18n/en/ext.php
+++ xExtension-YouTubeChannel2RssFeed/i18n/en/ext.php
@@ -15,20 +15,24 @@ return array(
             'bad_freshrss' => '`YouTubeChannel2RssFeed`required at least FreshRSS version `%s`. (You use FreshRSS version `%s`)',
         ),
         'shorts' => array(
+            'duration' => array(
+                'label' => 'Duration in seconds',
+                'hint' => 'If you set a duration greater than 0, all shorter videos will be identified as short, even if YouTube says it is not short.',
+            ),
             'label' =>  'YouTube Shorts',
-            'hint' => 'This setting is only taken if an instance URL is stored. If none is deposited, shorts are permitted',
+            'hint' => 'This setting is only taken if an instance URL is stored. If none is deposited, shorts are permitted.',
             'options' => array(
                 'default' => array(
                     'text' => 'Default',
-                    'title' => 'No check for YouTube Shorts and the feed entry is added to the database as normal',
+                    'title' => 'No check for YouTube Shorts and the feed entry is added to the database as normal.',
                 ),
                 'block' => array(
                     'text' => 'Block',
-                    'title' => 'If the new feed entry is recognized as a Youtube Short, it will not be added into the database',
+                    'title' => 'If the new feed entry is recognized as a Youtube Short, it will not be added into the database.',
                 ),
                 'mark_as_read' => array(
                     'text' => 'Mark as read',
-                    'title' => 'If the new feed entry is recognized as a Youtube Short, it will be marked as read',
+                    'title' => 'If the new feed entry is recognized as a Youtube Short, it will be marked as read.',
                 ),
             ),
         ),
