f64ae2b5b8a96fdf92866c5e0a518744645b5398
Clemens Neubauer <cn-tools@gmx.at>
2024-06-04 08:15:08 +0200

    New translations ext.php (Polish)

---
+++ xExtension-BlackList/i18n/pl/ext.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'black_list' => [
+        'blocked_patterns' => 'Wzory czarnej listy',
+        'warning' => [
+            'not_allowed_msg' => '"%s" jest zablokowanym wzorem. Aby uzyskać więcej informacji, skontaktuj się z administratorem.',
+        ],
+    ],
+];
