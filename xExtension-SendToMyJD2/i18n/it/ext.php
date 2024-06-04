927e5a733cb8c8455e226e5ca99c27d1ca80888f
Clemens Neubauer <cn-tools@gmx.at>
2024-06-04 08:16:08 +0200

    New translations ext.php (Italian)

---
+++ xExtension-SendToMyJD2/i18n/it/ext.php
@@ -0,0 +1,21 @@
+<?php
+
+return array(
+	'SendToMyJD2' => array(
+        'config' => array(
+            'email_label' => 'Email',
+            'password_label' => 'Password',
+            'password_placeholder' => 'Tragen sie hier das Passwort ein',
+            'password_danger' => 'ATTENZIONE: La password viene salvata come testo semplice nel file di configurazione dell\'utente!',
+            'device_label' => 'Dispositivo',
+            'data_label' => 'Dati',
+            'data_help' => 'È disponibile un aiuto dettagliato per la configurazione su GitHub. Segui il link:',
+            'data_help_lnk_txt' => 'Documentazione',
+            'testdata' => 'Invia dati di prova',
+            'testdata_hint' => 'Quando si fa clic su Salva, il seguente link di YouTube viene inviato una volta all\'istanza jDownloader definita: ',
+        ),
+        'install' => array(
+            'bad_freshrss' => 'SendToMyJD2 richiede almeno la versione FreshRSS `%s`. (Stai usando la versione FreshRSS `%s`)',
+        ),
+    ),
+);
