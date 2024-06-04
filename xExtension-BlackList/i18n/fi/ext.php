1cfb8a186d882936b969e29ce1212b69712b95ff
Clemens Neubauer <cn-tools@gmx.at>
2024-06-04 08:15:04 +0200

    New translations ext.php (Finnish)

---
+++ xExtension-BlackList/i18n/fi/ext.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'black_list' => [
+        'blocked_patterns' => 'Mustat listamallit',
+        'warning' => [
+            'not_allowed_msg' => '"%s" on lukittu kuvio. Ota yhteyttä järjestelmänvalvojaan saadaksesi lisätietoja.',
+        ],
+    ],
+];
