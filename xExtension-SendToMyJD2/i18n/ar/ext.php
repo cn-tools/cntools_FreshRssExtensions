3e81897896efbc7f33a62ad13ee6dfa34bcf7e0b
Clemens Neubauer <cn-tools@gmx.at>
2024-06-04 10:02:24 +0200

    New translations ext.php (Arabic)

---
+++ xExtension-SendToMyJD2/i18n/ar/ext.php
@@ -0,0 +1,21 @@
+<?php
+
+return array(
+	'SendToMyJD2' => array(
+        'config' => array(
+            'email_label' => 'البريد الإلكتروني',
+            'password_label' => 'كلمة المرور',
+            'password_placeholder' => 'Tragen sie hier das Passwort ein',
+            'password_danger' => 'تنبيه: يتم حفظ كلمة المرور كنص عادي في ملف تكوين المستخدم!',
+            'device_label' => 'الجهاز',
+            'data_label' => 'البيانات',
+            'data_help' => 'مساعدة التكوين التفصيلية متوفرة على GitHub. اتبع الرابط:',
+            'data_help_lnk_txt' => 'الوثائق',
+            'testdata' => 'إرسال بيانات الاختبار',
+            'testdata_hint' => 'عند النقر على الحفظ، يتم إرسال رابط يوتيوب التالي مرة واحدة إلى مثيل jDownloader المحدد: ',
+        ),
+        'install' => array(
+            'bad_freshrss' => 'SendToMyJD2 يتطلب على الأقل إصدار FreshRSS `%s`. (أنت تستخدم FreshRSS version `%s`)',
+        ),
+    ),
+);
