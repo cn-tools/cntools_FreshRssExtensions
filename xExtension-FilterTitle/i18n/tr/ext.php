b2b4024fbbb501afd2ecbf77e8f8d0a77a2bab2d
Clemens Neubauer <cn-tools@gmx.at>
2026-01-30 06:40:56 +0100

    New translations ext.php (Turkish) (#47)

---
+++ xExtension-FilterTitle/i18n/tr/ext.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+    'filter_title' => array(
+        'blacklist' => 'Kara liste',
+        'keywords_desc' => 'Her satır ayrı olarak kontrol edilir. Normal metinlere ek olarak, RegEx ifadeleri de tanımlanabilir. Bunlar, preg_match PHP işlevi kullanılarak değerlendirilebilmelidir.',
+        'mark_as_read' => 'Okundu olarak işaretleyin',
+        'mark_as_read_hint' => 'Bu onay kutusu işaretlenirse, yetkisiz girişler yine de veri tabanına girilecek, ancak okundu olarak işaretlenecek',
+        'priority_hint' => 'Kara listenin beyaz listeden daha yüksek önceliğe sahip olduğunu unutmayın!',
+        'warning' => [
+            'not_allowed_msg' => '"%s" yasaklanmış bir anahtar kelimedir. Daha fazla bilgi için yöneticinizle iletişime geçin.',
+        ],
+        'whitelist' => 'Beyaz liste',
+    ),
+);
